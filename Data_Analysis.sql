94302bfa053aa41c51b098a728da5b298d7437d6
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-05-05 23:45:36 -0600

    DA update

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -26,4 +26,29 @@ dept_manager.dept_no = departments.dept_no
 INNER JOIN Employees empl ON
 dept_manager.emp_no = empl.emp_no
 
+--4. List the department of each employee with the following information: 
+	--employee number, last name, first name, and department name
+	
+SELECT empl.emp_no, 
+	empl.last_name,
+	empl.first_name, 
+	departments.dept_name
+FROM Employees empl
+INNER JOIN dept_manager ON
+empl.emp_no = dept_manager.emp_no
+INNER JOIN Departments ON
+dept_manager.dept_no = departments.dept_no
+
+--5. List the first name, last name, and sex for employees whose first name is "Hercules" 
+	--and last names begin with 'B'.
+
+SELECT 
+empl.first_name,
+empl.last_name,
+empl.sex
+FROM Employees empl
+WHERE empl.first_name = 'Hercules'
+AND empl.last_name like 'B%'
+
+
 
