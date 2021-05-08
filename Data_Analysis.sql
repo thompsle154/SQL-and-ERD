83f08d526e4b8946c6a70b6abb552a9b8b91cbdb
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-05-07 23:41:48 -0600

    Data Analysis final code

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -50,5 +50,37 @@ FROM Employees empl
 WHERE empl.first_name = 'Hercules'
 AND empl.last_name like 'B%'
 
+--6. List all employees in the Sales and Development departments, including their employee number,
+	--last name, first name, and department name.
 
+SELECT
+empl.emp_no,
+empl.last_name,
+empl.first_name,
+departments.dept_name
+FROM Employees empl
+JOIN dept_emp ON 
+empl.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their 
+	--employee number, last name, first name, and department name
+	
+SELECT empl.emp_no, empl.last_name, empl.first_name, dept.dept_name
+FROM Employees empl
+JOIN dept_emp ON
+empl.emp_no = dept_emp.emp_no
+INNER JOIN departments dept ON
+dept_emp.dept_no = dept.dept_no
+WHERE dept.dept_name = 'Sales' OR
+	dept.dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names i.e. how many employees share each last name
+
+Select empl.last_name, count(empl.last_name)
+FROM Employees empl
+GROUP BY (empl.last_name)
+ORDER BY count(empl.last_name) desc
 
